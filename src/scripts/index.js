8e43587989bd03bd137d4e9041516b659b3b2e3f
githubscientist <sathish.datascientist@gmail.com>
2025-05-17 11:28:00 +0530

    selectors dom api

---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+// console.log(document.getElementsByTagName('p')[2]);
+
+console.log(document.getElementsByClassName('italics'));
+
+// console.log(document.getElementById('dom'));
+
+// console.log(document.getElementById('welcome').textContent);
