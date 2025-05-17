31d1a8314641b3c7ad5e167f0336b48c75bb5ac2
githubscientist <sathish.datascientist@gmail.com>
2025-05-17 12:05:27 +0530

    dom api --manipulation textContent

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,19 @@
 // console.log(document.getElementsByTagName('p')[2]);
 
-console.log(document.getElementsByClassName('italics'));
+// console.log(document.getElementsByClassName('italics'));
 
 // console.log(document.getElementById('dom'));
 
-// console.log(document.getElementById('welcome').textContent);
+// console.log(document.getElementById('welcome'));
+
+// let welcomeText = document.getElementById('welcome');
+
+// welcomeText.textContent = 'This page will help you understand how to manipulate the Document Object Model using JavaScript.';
+
+let paraItalics = document.getElementsByClassName('italics');
+let wordLengthElements = document.getElementsByClassName('wordLength');
+
+for (let index = 0; index < paraItalics.length; index++) {
+    // console.log(paraItalics[index].textContent.split(' ').length);
+    wordLengthElements[index].textContent = `Words: ${paraItalics[index].textContent.split(' ').length}`;
+}
