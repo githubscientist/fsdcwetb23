a2a592d3f4a632c22d5b09ee9897ef956c405741
githubscientist <sathish.datascientist@gmail.com>
2025-05-24 12:17:12 +0530

    rock paper scissor

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,32 @@
-let about = document.querySelector('.about');
+// chooses a random number index
+// use that to select randomly one of the choices rock, paper, scissor
+function generateComputerChoice() {
+    let choices = ['Rock', 'Paper', 'Scissor'];
 
-// about.innerText = 'Ok'
-// about.textContent = 'Ok';
+    let randomNumber = Math.floor(Math.random() * 3);
 
-about.innerHTML = `<p>Let's talk about innerText first.</p><p>Yes, that's correct!</p><p>Let's move on next to innerHTML!</p>`
+    return choices[randomNumber];
+}
+
+function getWinner(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) return 'Tie';
+    else if ((playerChoice == 'Rock' && computerChoice == 'Scissor') || (playerChoice == 'Scissor' && computerChoice == 'Paper') || (playerChoice == 'Paper' && computerChoice == 'Rock')) {
+        return 'You Win!';
+    } else {
+        return 'Computer Wins!';
+    }
+}
+
+function play(playerChoice) {
+    let pChoice = document.getElementById('pChoice');
+    pChoice.textContent = playerChoice;
+
+    let computerChoice = generateComputerChoice();
+
+    let cChoice = document.getElementById('cChoice');
+    cChoice.textContent = computerChoice;
+
+    let winner = getWinner(playerChoice, computerChoice);
+    let result = document.getElementById('result');
+    result.textContent = winner;
+}
