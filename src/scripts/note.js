7e032930333c9ea0104f7b2f3050baac02c7db92
githubscientist <sathish.datascientist@gmail.com>
2025-06-01 13:19:02 +0530

    delete done

---
+++ src/scripts/note.js
@@ -0,0 +1,61 @@
+let noteID;
+
+async function readParams(id) {
+    const params = new URLSearchParams(window.location.search);
+    return params.get(id);
+}
+
+async function fetchNote(noteID) {
+    try {
+        const response = await fetch(`https://683bdc3428a0b0f2fdc593ef.mockapi.io/notes/${noteID}`);
+        const note = await response.json();
+        return note;
+    } catch (error) {
+        console.log(error.message);
+        return {};
+    }
+}
+
+function populate(note) {
+    // get all the elements
+    let title = document.querySelector('.title');
+    let description = document.querySelector('.description');
+    let isImportant = document.querySelector('.isImportant');
+    let createdAt = document.querySelector('.createdAt');
+    let updatedAt = document.querySelector('.updatedAt');
+
+    // set the contens of the elements
+    title.textContent = note.title;
+    description.textContent = note.description;
+    // isImportant.textContent = note.important;
+    createdAt.textContent = `Created At: ${note.createdAt}`;
+    updatedAt.textContent = `Updated At: ${note.updatedAt}`;
+}
+
+(async () => {
+    noteID = await readParams('id');
+    const note = await fetchNote(noteID);
+    populate(note);
+})();
+
+function deleteNote() {
+    let option = confirm('Are you sure?');
+    if (option) {
+        fetch(`https://683bdc3428a0b0f2fdc593ef.mockapi.io/notes/${noteID}`, {
+            method: 'DELETE',
+        })
+            .then(() => {
+                alert('note deleted');
+
+                window.location.href = '/';
+            })
+            .catch(() => {
+                alert('note delete failed');
+            })
+    } else {
+        return;
+    }
+}
+
+let deleteButton = document.querySelector('.delete');
+deleteButton.addEventListener('click', deleteNote);
