c79a4f3e1ae8a817ea9731ad642b9c5c25c49656
githubscientist <sathish.datascientist@gmail.com>
2025-06-01 12:19:34 +0530

    notes clickable

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,49 @@
+let notes = [];
+let notesDiv = document.querySelector('.notes');
+
 async function fetchNotes() {
     console.log('Fetching notes...');
 
     try {
         // call the api
         const response = await fetch('https://683bdc3428a0b0f2fdc593ef.mockapi.io/notes');
-        const notes = await response.json();
-
-        console.log(notes);
+        return await response.json();
     } catch (error) {
         console.log(error.message);
     }
 }
 
-fetchNotes();
+async function populate(notes) {
+    // create an unordered list element (ul)
+    const ul = document.createElement('ul');
+
+    // loop over the notes array
+    notes.forEach((note) => {
+        // and for each of the notes object
+        // create an li item
+        const li = document.createElement('li');
+
+        // create an anchor tag
+        const a = document.createElement('a');
+
+        // set the content of the anchor tag to note.title
+        a.textContent = note.title;
+
+        // set the href attribute of the anchor tag
+        a.setAttribute('href', `/note.html?id=${note.id}`);
+
+        // append the anchor tags to the li item
+        li.append(a);
+
+        // append the li item to the ul list
+        ul.append(li);
+    })
+
+    // after coming out of the loop, append the ul list to the notesDiv
+    notesDiv.append(ul);
+}
+
+(async () => {
+    notes = await fetchNotes();
+    await populate(notes);
+})();
