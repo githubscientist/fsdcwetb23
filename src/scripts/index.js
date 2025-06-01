180233e18e5e73e20a5dee5a7c7d04ad30793ec0
githubscientist <sathish.datascientist@gmail.com>
2025-06-01 10:16:42 +0530

    error handling, async await, try catch

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,23 +1,21 @@
 let todosButton = document.querySelector(".todosButton");
 
-function fetchTodos() {
+async function fetchTodos() {
     console.log('Fetching Todos...');
-    // try {
-    //     console.log(fruits[0]); // Error Handling: Application breaks
-    // } catch (error) {
-    //     console.log(error.message);
-    // }
-    fetch('https://683aa2ef43bb370a86733ba3.mockapi.io/todos')
-        .then((data) => {
-            console.log('Fetched Todos...');
-            return data.json();
-        })
-        .then((todos) => {
+    try {
+        const response = await fetch('https://683aa2ef43bb370a86733ba3.mockapi.io/todos');
+        console.log('Fetched Todos...');
+
+        try {
+            const todos = await response.json();
+
             console.log(todos);
-        })
-        .catch((error) => {
+        } catch (error) {
             console.log(error.message);
-        })
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
 todosButton.addEventListener('click', fetchTodos);
